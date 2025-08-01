f819152949ff9ebd3996fa6c42f669bea2e7780a
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-08-01 16:27:18 -0300

    first commit

---
+++ personagem.js
@@ -0,0 +1,22 @@
+//1
+let nome = "lyra";
+let classe = "guerreira";
+let nivel = 14;
+let vida = 100;
+let ouro = 100;
+let xp = 1050;
+
+console.log(`Olá! Eu sou a ${nome}, da classe ${classe}. Possuo o nivel ${nivel} com ${vida} pontos de vida, no meu inventario tenho ${ouro} de ouro e ${xp} de xp`);
+
+//2
+let NOME_ARMA = "cajado de Batalha";   
+let DANO_BASE = 100;  
+let NOME_ARMADURA = "Armadura acolchoada" ;
+let DEFESA_BASE = 100;
+
+console.log(`⚔ Arma: ${NOME_ARMA}`);
+console.log(`💣 Dano Base: ${DANO_BASE}`);
+console.log(`🛡 Armadura: ${NOME_ARMADURA}`);
+console.log(`🏹 Defesa: ${DEFESA_BASE}`);
+
+//3
